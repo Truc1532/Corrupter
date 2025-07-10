7b829dfa875cb203160d6bb07f3c673199ec7c53
Arthur <165717741+Truc1532@users.noreply.github.com>
2025-07-10 17:13:58 +0200

    Update build.pl

--- build.pl
+++ build.pl
@@ -5,9 +5,10 @@ use File::Path qw(make_path);
 my $SRC_DIR = "src";
 my $CC = "gcc";
 my $FINAL_DIR = "/usr/bin";
+my $FLAGS = "-std=c99 -pedantic"
 
 make_path($SRC_DIR);
 make_path($FINAL_DIR);
 
-system("$CC $SRC_DIR/utils.c $SRC_DIR/main.c -o corrupter");
+system("$CC $FLAGS $SRC_DIR/utils.c $SRC_DIR/main.c -o corrupter");
 system("mv corrupter $FINAL_DIR/");
